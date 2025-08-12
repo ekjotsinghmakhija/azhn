1b74d417b52141a9881f74c96aea286deadc67d9
Ekjot Singh <ekjotmakhija@gmail.com>
2025-08-12 22:53:08 +0530

    src folder for project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export type {
+  FontOptions as Font,
+  Weight as FontWeight,
+  FontStyle,
+} from './font.js'
+export type { Locale } from './language.js'
+
+export * from './azhn.js'
+export { default } from './azhn.js'
