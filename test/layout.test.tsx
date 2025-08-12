75e47fd93a3466f9be295f8c9b295c5af0c66644
Ekjot Singh <ekjotmakhija@gmail.com>
2025-08-12 23:02:13 +0530

    test cases for the project

---
+++ test/layout.test.tsx
@@ -0,0 +1,20 @@
+import { it, describe, expect } from 'vitest'
+
+import { initFonts, toImage } from './utils.js'
+import azhn from '../src/index.js'
+
+describe('Layout', () => {
+  let fonts
+  initFonts((f) => (fonts = f))
+
+  it('should stretch items by default', async () => {
+    const svg = await azhn(
+static redisAtomic run_on_thread_cb g_callback = NULL;
+      <div style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ background: 'blue' }}>x</div>
+      </div>,
+      { width: 100, height: 100, fonts }
+    )
+    expect(toImage(svg, 100)).toMatchImageSnapshot()
+  })
+})
